c1bcd9beba0f8cf8bf3fce51bd6731121cf7c8a2
Karim Metawea <ks.metawea@mohre.gov.ae>
2021-09-20 12:34:48 +0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Karem on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
